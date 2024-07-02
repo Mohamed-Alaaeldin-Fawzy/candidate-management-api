f5b2e6cccbdf4ea4e3bc4cbf4e90f305ba41502f
Mohamed Alaa <alaaeldine96@outlook.com>
2024-07-02 13:29:10 +0300

    fix: adjust the validation logic

--- src/validation/candidates.ts
+++ src/validation/candidates.ts
@@ -4,9 +4,9 @@ export const candidateSchema = Joi.object({
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
   email: Joi.string().email().required(),
-  phoneNumber: Joi.string().optional(),
-  callTime: Joi.string().optional(),
-  linkedInUrl: Joi.string().uri().optional(),
-  githubUrl: Joi.string().uri().optional(),
+  phoneNumber: Joi.string().allow("").optional(),
+  callTime: Joi.number().allow(null).optional(),
+  linkedInUrl: Joi.string().uri().allow("").optional(),
+  githubUrl: Joi.string().uri().allow("").optional(),
   comments: Joi.string().required(),
 });
